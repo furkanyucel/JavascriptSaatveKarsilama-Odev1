a0ed9ddaa0345397bf022c2d8c66d6c9c84df195
Furkan Yücel <47148709+furkanyucelux@users.noreply.github.com>
2022-05-09 17:43:55 +0300

    Javascript dosyası yüklendi

---
+++ js/clock.js
@@ -0,0 +1,12 @@
+let user = prompt("Adınız nedir?");
+let myName = document.querySelector('#myName');
+myName.innerHTML = ` ${user} `;
+
+function showTime() {
+let d = new Date();
+let daysOfWeek = ["Pazar","Pazartesi","Salı","Çarşamba","Perşembe","Cuma","Cumartesi"];
+let time = document.querySelector('#myClock')
+
+time.innerHTML = `${d.getHours()}:${d.getMinutes()}:${d.getSeconds()} ${daysOfWeek[d.getUTCDay()]}` setTimeout(showTime, 1000); }
+
+showTime();
