50bf3ccbcc6da60a298a5c9ac2e687006ac0db2d
Furkan Yücel <47148709+furkanyucelux@users.noreply.github.com>
2022-05-09 17:53:50 +0300

    Update clock.js

--- js/clock.js
+++ js/clock.js
@@ -7,6 +7,7 @@ let d = new Date();
 let daysOfWeek = ["Pazar","Pazartesi","Salı","Çarşamba","Perşembe","Cuma","Cumartesi"];
 let time = document.querySelector('#myClock')
 
-time.innerHTML = `${d.getHours()}:${d.getMinutes()}:${d.getSeconds()} ${daysOfWeek[d.getUTCDay()]}` setTimeout(showTime, 1000); }
-
+time.innerHTML = `${d.getHours()}:${d.getMinutes()}:${d.getSeconds()} ${daysOfWeek[d.getUTCDay()]}` 
+setTimeout(showTime, 1000); 
+}
 showTime();
